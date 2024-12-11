1fc2ab31ba5470fa90aa1a2ad9d57faea291dfa4
vina786 <vinay76b@gmail.com>
2024-12-11 13:29:08 +0530

    Add files via upload

---
+++ protectedaccess.java
@@ -0,0 +1,11 @@
+//protected accessifier can be used in static methods
+
+public class protectedaccess {
+     protected static void s()
+    {
+        System.out.println("hello vinay");
+    }
+    public static void main(String args[]){
+        s();
+    }
+}
