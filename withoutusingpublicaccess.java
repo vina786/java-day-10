dd1050e48ef116196fac345da2f33a8bcaecf4ed
vina786 <vinay76b@gmail.com>
2024-12-11 13:30:16 +0530

    Add files via upload

---
+++ withoutusingpublicaccess.java
@@ -0,0 +1,14 @@
+//without using public accessfier can also use and access
+public class withoutusingpublicaccess {
+    static String s()
+    {
+        return "hello vinay";
+    }
+    public static void main(String args[])
+    {
+    String res=s();
+    System.out.println(res);
+
+    }
+    
+}
