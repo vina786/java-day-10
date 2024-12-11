8a0e6b9a94253300b2992cc91a92ae237a7ef929
vina786 <vinay76b@gmail.com>
2024-12-11 13:28:36 +0530

    Add files via upload

---
+++ nonvoid.java
@@ -0,0 +1,12 @@
+// static with non void
+public class nonvoid {
+    public static String s()
+    {
+        return "hello vinay";
+    }
+    public static void main(String args[])
+    {
+    String res =s();
+    System.out.println(res);
+    }
+}
