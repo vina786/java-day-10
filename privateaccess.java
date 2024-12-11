cd083a0f4c3f6676ab99728854b4281721e01a59
vina786 <vinay76b@gmail.com>
2024-12-11 13:28:54 +0530

    Add files via upload

---
+++ privateaccess.java
@@ -0,0 +1,12 @@
+ //private access can be used in static
+ class privateaccess {
+  private  static void s()
+    {
+        System.out.println("hello vinay");
+    }
+    public static void main(String args[]){
+        s();
+    }
+}
+    
+    
