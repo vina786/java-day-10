17a7cff997dfebe21b38215f4e1537ba28e7c9ad
vina786 <vinay76b@gmail.com>
2024-12-11 13:26:58 +0530

    Add files via upload

---
+++ creatingclass.java
@@ -0,0 +1,13 @@
+class a{
+    int a1=5;
+}
+public class creatingclass
+{
+    public static void main(String args[])
+    {
+        // r is instance variable
+        a r=new a();// new is used for creation  of instance variable
+        System.out.println(r.a1);
+    }
+    
+    }
