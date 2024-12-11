7884af6f6db30fc8b9933bef15fcd8dc3e317832
vina786 <vinay76b@gmail.com>
2024-12-11 13:29:57 +0530

    Add files via upload

---
+++ static1.java
@@ -0,0 +1,10 @@
+//without creating objects we can use the static methods
+public class static1 {
+    public static void s()
+    {
+        System.out.println("hello vinay");
+    }
+    public static void main(String args[]){
+        s();
+    }
+}
